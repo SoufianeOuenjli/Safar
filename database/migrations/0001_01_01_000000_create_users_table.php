cd0b534b1fc6077387fb5657f8864539827f02f3
unknown <sarachakour45@gmail.com>
2025-02-27 19:51:09 +0000

    Ajout de la migration de la base de donnees

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -29,7 +29,8 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->unsignedBigInteger('user_id')->nullable()->index(); // Utilisation de unsignedBigInteger pour assurer la compatibilité
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
